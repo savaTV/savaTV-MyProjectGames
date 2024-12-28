4396ecb901d3d96bcc18b44339f998efb7894d2d
Sava <zivaataga@gmail.com>
2024-12-28 20:21:12 +0300

    mod

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
+        GameWindow window = new GameWindow();
+        window.setVisible(true);
         GameWorld gameWorld = new GameWorld();
         gameWorld.heroName();
         gameWorld.startGame();
